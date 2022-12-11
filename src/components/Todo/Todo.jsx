b411875bf1dcb4e83e78ea1b9b9f35d4a7bffcab
Daekuen <daekuenhan@gmail.com>
2022-12-11 12:06:23 +0900

    feat: todo list css

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { BsTrashFill } from 'react-icons/bs';
+import styles from './Todo.module.css';
 
 export default function Todo({ todo, onUpdate, onDelete }) {
   const { title, status } = todo;
@@ -10,17 +11,22 @@ export default function Todo({ todo, onUpdate, onDelete }) {
     onUpdate({ ...todo, status });
   };
   return (
-    <li>
+    <li className={styles.todo}>
       <input
+        className={styles.checkbox}
         type="checkbox"
         id="checkbox"
         checked={status === 'completed'}
         onChange={handleChange}
       />
-      <label htmlFor="checkbox">{title}</label>
-      <button onClick={handleDelete}>
-        <BsTrashFill />
-      </button>
+      <label htmlFor="checkbox" className={styles.title}>
+        {title}
+      </label>
+      <span className={styles.icon}>
+        <button onClick={handleDelete} className={styles.button}>
+          <BsTrashFill />
+        </button>
+      </span>
     </li>
   );
 }
