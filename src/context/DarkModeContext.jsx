8bd319a5047ff911ea452480b6a5d18cd00d0bc8
Daekuen <daekuenhan@gmail.com>
2022-12-11 16:41:18 +0900

    feat: darkMode 구현

---
+++ src/context/DarkModeContext.jsx
@@ -0,0 +1,38 @@
+import { createContext, useContext, useState, useEffect } from 'react';
+
+export const DarkModeContext = createContext();
+
+export function DarkModeProvider({ children }) {
+  const [darkMode, setDarkMode] = useState(false);
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    updateDarkMode(!darkMode);
+  };
+
+  useEffect(() => {
+    const isDark =
+      localStorage.theme === 'dark' ||
+      (!('theme' in localStorage) &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches);
+    setDarkMode(isDark);
+    updateDarkMode(isDark); // HTML에 className을 추가하고, localStorage에 theme 상태값 저장.
+  }, []);
+
+  return (
+    <DarkModeContext.Provider value={{ darkMode, toggleDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+}
+
+function updateDarkMode(darkMode) {
+  if (darkMode) {
+    document.documentElement.classList.add('dark');
+    localStorage.theme = 'dark';
+  } else {
+    document.documentElement.classList.remove('dark');
+    localStorage.theme = 'light';
+  }
+}
+
+export const useDarkMode = () => useContext(DarkModeContext);
